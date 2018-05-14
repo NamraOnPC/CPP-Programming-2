de091d08a5e06e7a20c0f63f9d71e9bfa0c456bc
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-05-14 14:55:24 -0400

    Add files via upload

---
+++ Workshop1/main.cpp
@@ -0,0 +1,45 @@
+//Name :- Namra Fanse
+// Student ID :- 112219175
+//Student Email :- nrfanse@myseneca.ca
+// Lab 1 OOP345
+
+
+
+#include <iostream>
+#include "Process.h"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	extern int STORED;	
+
+	cout << "Command Line :";
+
+	for (int i = 0; i < argc; i++){
+
+		cout << " " << argv[i];
+
+	}
+
+	cout << endl;
+
+	if (argc <= 1){
+
+		cout << "Insufficient number of arguments ( min 1 )" << endl;
+
+		return 1;
+
+	}else{
+
+		cout << "Maximum number of characters to be stored : " << STORED << endl;
+
+		for (int i = 1; i < argc; i++){
+
+			process(argv[i]);
+
+		}
+
+		return 0;
+	}
+
+}
