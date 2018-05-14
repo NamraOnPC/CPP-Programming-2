67be0191b6682a8475c06ccbc2256a3f60a5a27f
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-05-14 14:53:10 -0400

    Add files via upload

---
+++ Workshop1/Process.cpp
@@ -0,0 +1,18 @@
+//Name :- Namra Fanse
+// Student ID :- 112219175
+//Student Email :- nrfanse@myseneca.ca
+// Lab 1 OOP345
+
+
+#include "Process.h"
+#include "CString.h"
+#include <iostream>
+using namespace std;
+
+	void process(const char *string) {
+
+		w1::CString cstring(string);
+
+		cout << cstring << endl;
+
+	}
