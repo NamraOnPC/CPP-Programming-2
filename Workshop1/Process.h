067b1c32c423cbcae25c3fc883844f0a790223de
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-05-14 14:54:13 -0400

    Add files via upload

---
+++ Workshop1/Process.h
@@ -0,0 +1,13 @@
+//Name :- Namra Fanse
+// Student ID :- 112219175
+//Student Email :- nrfanse@myseneca.ca
+// Lab 1 OOP345
+
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+void process(const char *string);
+
+
+#endif // !PROCESS_H
