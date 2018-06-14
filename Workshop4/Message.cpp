4c4e555f7dda00d7e6889d661ef6f5d39b169b5b
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-06-13 20:52:17 -0400

    Message module

---
+++ Workshop4/Message.cpp
@@ -0,0 +1,92 @@
+//Name :- Namra Fanse
+//Student No :- 112219175
+//Email :- nrfanse@myseneca.ca
+//LAB 4 OOP345
+
+
+#include <sstream>
+#include "Message.h"
+using namespace std;
+
+namespace sict {
+
+	Message::Message() {}
+
+	Message::Message(std::ifstream& in, char c) {
+
+		string data , foo;
+
+		getline(in, data, c);
+
+		istringstream istr(data);
+
+		if (_user.empty()) {
+
+			istr >> _user;
+
+			if ((int)_user.find('@') > 0) {
+
+				_user.clear();
+
+			}
+
+		}
+
+		if (_reply.empty()) {
+
+			istr >> _reply;
+
+			if ((int)_reply.find('@') < 0) {
+
+				istr.seekg(0, ios::beg);
+
+				istr >> foo;
+
+				getline(istr, _tweets);
+
+				_reply.clear();
+
+			}
+
+		}
+
+		if (_tweets.empty()) {
+
+			getline(istr, _tweets);
+
+		}
+
+		_reply.erase(0, 1);
+		_tweets.erase(0,1);
+		istr.clear();
+
+	}
+	
+
+	void Message::display(std::ostream& ostr) const {
+
+		if (_tweets != "") {
+
+			ostr << "Message" << endl;
+
+			ostr << "\tUser\t:\t" << _user << endl;
+
+			if (!_reply.empty()) {
+
+				ostr << "\tReply\t:\t" << _reply << endl;
+
+			}
+
+			ostr << "\tTweet\t:\t" << _tweets << endl;
+
+		}
+
+	}
+
+	bool Message::empty() const {
+
+		return _user.empty() || _tweets.empty();
+
+	}
+
+}
---
+++ Workshop4/Message.h
@@ -0,0 +1,40 @@
+//Name :- Namra Fanse
+//Student No :- 112219175
+//Email :- nrfanse@myseneca.ca
+//LAB 4 OOP345
+
+#ifndef _MESSAGE_H
+#define _MESSAGE_H
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+namespace sict {
+
+	class Message {
+
+	private:
+
+		std::string _user;
+
+		std::string _reply;
+
+		std::string _tweets;
+
+	public:
+
+		Message();
+
+		bool empty() const;
+
+		Message(std::ifstream& in, char c);
+
+		void display(std::ostream& ostr) const;
+
+
+	};
+
+}
+
+#endif // !_MESSAGE_H
