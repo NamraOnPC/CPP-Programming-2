78e139f7dde8688483044775128a27b7fc403571
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-06-13 20:51:59 -0400

    Notifications module

---
+++ Workshop4/Notifications.cpp
@@ -0,0 +1,85 @@
+//Name :- Namra Fanse
+//Student No :- 112219175
+//Email :- nrfanse@myseneca.ca
+//LAB 4 OOP345
+
+#include <string>
+#include "Notifications.h"
+using namespace std;
+
+namespace sict {
+
+	Notifications::Notifications() : _message(nullptr) , _counter(0) {}
+
+	Notifications::Notifications(std::ifstream& istr) : _message(nullptr), _counter(0) {
+
+		std::string data;
+
+		while (getline(istr, data)) {
+
+			_counter++;
+
+		}
+
+		istr.clear();
+
+		istr.seekg(0, std::ios::beg);
+
+		if (_message == nullptr) {
+
+			_message = new Message[_counter];
+
+		}
+
+		for (int x = 0; x < _counter; x++) {
+
+			_message[x] = Message(istr, MAX_MSG_SIZE);
+
+		}
+
+
+	}
+
+	Notifications::Notifications(Notifications&& org) {
+
+		*this = move(org);
+
+	}
+
+	Notifications&& Notifications::operator=(Notifications&& org) {
+
+		if (&org != this) {
+
+			delete[] _message;
+
+			_message = org._message;
+			_counter = org._counter;
+
+			org._message = nullptr;
+			org._counter = 0;
+
+		}
+
+		return move(*this);
+
+	}
+
+	Notifications::~Notifications() {
+
+		delete[] _message;
+
+		_message = nullptr;
+
+	}
+
+	void Notifications::display(std::ostream& ostr) const {
+
+		for (int i = 0; i < _counter; i++) {
+			
+			_message[i].display(ostr);
+
+		}
+
+	}
+
+}
---
+++ Workshop4/Notifications.h
@@ -0,0 +1,54 @@
+//Name :- Namra Fanse
+//Student No :- 112219175
+//Email :- nrfanse@myseneca.ca
+//LAB 4 OOP345
+
+
+#ifndef _NOTIFICATIONS_H
+#define _NOTIFICATIONS_H
+#include "Message.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+namespace sict {
+
+	const int MAX_MSG_SIZE = 10;
+
+	const char DELIMITER = '\n';
+
+	class Notifications {
+
+	private:
+
+		Message * _message;
+
+		int _counter;
+
+	public:
+
+		Notifications();
+
+		Notifications(std::ifstream&);
+
+	//	Notifications(Notifications&) = delete;
+
+		Notifications(Notifications&&);
+
+	//	Notifications& operator=(Notifications&) = delete;
+
+		Notifications&& operator=(Notifications&&);
+
+		~Notifications();
+
+		void display(std::ostream& ostr) const;
+
+
+	};
+
+
+}
+
+
+#endif // !_NOTIFICATIONS_H
+
