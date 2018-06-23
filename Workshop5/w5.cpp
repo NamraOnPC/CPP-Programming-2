a221c373617c15d12e31f9f8d9fa605ef9f1f1d6
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2018-06-23 01:33:47 -0400

    Add files via upload

---
+++ Workshop5/w5.cpp
@@ -0,0 +1,64 @@
+// Workshop 5 - Lambda Expression
+// w5.cpp
+// Chris Szalwinski
+// 2018-05-23
+
+#include <iostream>
+#include "Grades.h"
+#include "Letter.h"
+using namespace sict;
+
+int main(int argc, char* argv[]) {
+	std::cout << "Command Line : ";
+	for (int i = 0; i < argc; i++) {
+		std::cout << argv[i] << ' ';
+	}
+	std::cout << std::endl;
+
+	if (argc == 1) {
+		std::cerr <<
+			"\n*** Insufficient number of arguments ***\n";
+		std::cerr << "Usage: " << argv[0] << " fileName \n";
+		return 1;
+	}
+	else if (argc != 2) {
+		std::cerr << "\n*** Too many arguments ***\n";
+		std::cerr << "Usage: " << argv[0] << " fileName \n";
+		return 2;
+	}
+
+
+	sict::Grades grades(argv[1]);
+	// lambda expression for converting to 
+	auto letter = [](double grades_)->std::string
+	{
+		std::string letterGrade;
+
+		if (grades_ >= 90)
+			letterGrade = "A+";
+		else if (grades_ >= 80)
+			letterGrade = "A";
+		else if (grades_ >= 75)
+			letterGrade = "B+";
+		else if (grades_ >= 70)
+			letterGrade = "B";
+		else if (grades_ >= 65)
+			letterGrade = "C+";
+		else if (grades_ >= 60)
+			letterGrade = "C";
+		else if (grades_ >= 55)
+			letterGrade = "D+";
+		else if (grades_ >= 50)
+			letterGrade = "D";
+		else
+			letterGrade = "F";
+		return letterGrade;
+	};
+
+	grades.displayGrades(std::cout, letter);
+
+	// report the error message
+	std::cerr << argv[0] << ": Failed to open file "
+		<< argv[1] << "\n";
+	return 2;
+}
